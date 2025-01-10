6f3437ff7c2db522e700067484be6f405fd939ea
Harsh Raj Kumar <harsh726181@gmail.com>
2025-01-11 00:04:18 +0530

    Fix fav ico and projects

--- src/Data/ProjectData.jsx
+++ src/Data/ProjectData.jsx
@@ -1,52 +1,77 @@
-import imgL from '../assets/leetcodeProfile.png'
-import imgCn from '../assets/codingNinjaProfile.png'
-import imgPf from '../assets/portfolio.png'
-import imgWa from '../assets/Weatherapp.png'
+import imgL from "../assets/leetcodeProfile.png";
+import imgNetFlix from "../assets/netflixClone.png";
+import imgBlogApp from "../assets/blogapp.png";
+import imgTicTacToe from "../assets/TicTacToe.png";
+import imgTicTodoList from "../assets/TodoApp.png";
+import imgfoodApp from "../assets/onlineFood.png";
+import imgPf from "../assets/portfolio.png";
+import imgWa from "../assets/Weatherapp.png";
 const ProjectData = [
-     {
-        name:"Netflix-Clone",
-        discription:"Made clone of netfilx.",
-        technologyUsed : [ "React","Redux","Node.js","Firebase"],
-        imgink: imgL,
-     },
-     {
-      name:"Portfolio",
-      discription:"This is portfolio that I created using react.",
-      technologyUsed : [ "React","Tailwind CSS"],
-      imgink: imgPf,
-   },
-     {
-        name:"Weather-APP",
-        discription:"Built using Open weather API",
-        technologyUsed : [ "React","CSS","API"],
-        imgink: imgWa,
-        url:"https://hrweather.netlify.app/",
-        gitUrl:"https://github.com/HarshKRa/wheatherapp",
-     },
-     {
-      name:"Tic Tac Toe",
-      discription:"Bilt a game using JavaScript",
-      technologyUsed : [ "HTML","CSS","JavaScript"],
-      imgink: imgL,
-      url:"https://harshtictactoegame.netlify.app/",
-      gitUrl:"https://github.com/HarshKRa/TicTacToeGame",
-   },
-     {
-        name:"Digital-Clock",
-        discription:"Built using Date and time function.",
-        technologyUsed : [ "HTML","CSS","JavaScript"],
-        imgink: imgL,
-     },
+  {
+    name: "Netflix-Clone",
+    discription: "Made clone of netfilx.",
+    technologyUsed: ["React", "Redux", "Node.js", "Firebase"],
+    imgink: imgNetFlix,
+    url: "https://moviewebsiteby.netlify.app/login",
+    gitUrl: "https://github.com/HarshKRa/netflix_clone",
+  },
+  {
+    name: "Portfolio",
+    discription: "This is portfolio that I created using react.",
+    technologyUsed: ["React", "Tailwind CSS"],
+    imgink: imgPf,
+    url: "https://rajharsh.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/My-Portfolio",
+  },
+  {
+    name: "Blog writing App",
+    discription: "Write here which you want",
+    technologyUsed: ["React", "Node.js", "Mongodb"],
+    imgink: imgBlogApp,
+    url: "https://harshblogapp.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/BlogAPPUI",
+  },
+  {
+    name: "Weather-APP",
+    discription: "Built using Open weather API",
+    technologyUsed: ["React", "CSS", "API"],
+    imgink: imgWa,
+    url: "https://hrweather.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/wheatherapp",
+  },
+  {
+    name: "Todo App",
+    discription: "Make your notes here",
+    technologyUsed: ["React", "CSS"],
+    imgink: imgTicTodoList,
+    url: "https://makeyournotetodo.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/todoapp",
+  },
+  {
+    name: "Tic Tac Toe",
+    discription: "Bilt a game using JavaScript",
+    technologyUsed: ["HTML", "CSS", "JavaScript"],
+    imgink: imgTicTacToe,
+    url: "https://harshtictactoegame.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/TicTacToeGame",
+  },
+  {
+    name: "Digital-Clock",
+    discription: "Built using Date and time function.",
+    technologyUsed: ["HTML", "CSS", "JavaScript"],
+    imgink: imgL,
+    url: "https://hrweather.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/wheatherapp",
+  },
 
-     {
-      name:"Online Food",
-      discription:"Built using HTML CSS Responsive application",
-      technologyUsed : [ "HTML","CSS"],
-      imgink: imgL,
-      url:"https://harshfood.netlify.app/",
-      gitUrl:"https://github.com/HarshKRa/OnlineFood"
-   }
-     
-]
+  {
+    name: "Online Food",
+    discription: "Built using HTML CSS Responsive application",
+    technologyUsed: ["HTML", "CSS"],
+    imgink: imgfoodApp,
+    url: "https://harshfood.netlify.app/",
+    gitUrl: "https://github.com/HarshKRa/OnlineFood",
+  },
+];
 
-export default ProjectData
+export default ProjectData;
